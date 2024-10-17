74244e7924b7f939a8c50ff91b10e5502ed735fe
yaren <yaren@DESKTOP-9U2BG79>
2024-10-18 01:39:53 +0300

    Add project files.

---
+++ 10 Sayisinin 3'e Bolumunden Kalanini Bulan CSharp Kodu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Sayisinin_3_e_Bolumunden_Kalanini_Bulan_CSharp_Kodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Sayı = 10;
+
+            int Bölen = 3;
+
+            int Kalan = Sayı % Bölen;
+
+            Console.WriteLine("10 Sayısının 3'e Bölümünden Kalan: " + Kalan);
+
+            Console.ReadKey();
+
+        }
+    }
+}
